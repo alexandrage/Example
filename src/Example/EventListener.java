e40d5ac3e571045d40a645f2612f30314d85ac44
alexandrage <alexandrage@mail.ru>
2017-04-03 03:57:33 +0300

    up

--- src/Example/EventListener.java
+++ src/Example/EventListener.java
@@ -4,25 +4,18 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.material.SpawnEgg;
 
 public class EventListener implements Listener {
 	Main plugin;
 	public EventListener(Main instance) {
 		this.plugin = instance;
 	}
-	
-    @EventHandler
-    public void on(EntityChangeBlockEvent e) {
-        if (e.getEntityType().equals(EntityType.FALLING_BLOCK)) {
-            e.setCancelled(true);
-        }
-    }
-    
-    @EventHandler
-    public void on(PlayerInteractEvent e) {
-	SpawnEgg egg = new SpawnEgg(EntityType.HORSE);
-        e.getPlayer().getInventory().setItemInMainHand(egg.toItemStack());
-    }
+
+	@EventHandler
+	public void on(EntityChangeBlockEvent e) {
+		if (e.getEntityType().equals(EntityType.FALLING_BLOCK)) {
+			e.setCancelled(true);
+			e.getBlock().getState().update();
+		}
+	}
 }
--- src/Example/Main.java
+++ src/Example/Main.java
@@ -1,11 +1,33 @@
 package Example;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+	
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(new EventListener(this), this);
 		getCommand("example").setExecutor(new CommandListener(this));
 		new Scheduler(this).runTaskTimer(this, 0, 20);
+		if(!getConfig().contains("example")) {
+			Config cfg = new Config();
+			cfg.setI(10);
+			cfg.setS("string");
+			cfg.setL(Arrays.asList(new String[]{"one", "two"}));
+			Map<String, String> m = new HashMap<String, String>();
+			m.put("one", "on");
+			m.put("two", "tw");
+			cfg.setM(m);
+			getConfig().set("example", cfg);
+			saveConfig();
+		}
+	}
+
+	static {
+    	ConfigurationSerialization.registerClass(Config.class, "Example");
 	}
 }
