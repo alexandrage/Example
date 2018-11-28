88f44f4f01d7cf1b4a95e3664eb306da40df553a
alexandrage <alexandrage@mail.ru>
2018-11-28 13:15:38 +0300

    .

--- src/Example/db/H2.java
+++ src/Example/db/H2.java
@@ -8,13 +8,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 public class H2 implements Database {
 	private Connection conn;
-	private Statement statmt;
-	private PreparedStatement preparedStatement = null;
 
 	public void loadDriver(String u) {
 		try {
@@ -34,9 +31,8 @@ public class H2 implements Database {
 		try {
 			Class.forName("org.h2.Driver");
 			conn = DriverManager.getConnection("jdbc:h2://" + url + ";mode=MySQL", "sa", "");
-			statmt = conn.createStatement();
-			statmt.execute(
-					"CREATE TABLE IF NOT EXISTS `users` (`user` varchar(255) PRIMARY KEY,`time` varchar(255) NOT NULL)");
+			conn.createStatement().execute(
+					"CREATE TABLE IF NOT EXISTS `users` (`user` varchar(16) PRIMARY KEY,`time` varchar(255) NOT NULL)");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -59,13 +55,13 @@ public class H2 implements Database {
 	@Override
 	public ArrayList<String> select(String user) {
 		try {
-			preparedStatement = conn.prepareStatement("SELECT * FROM users WHERE user = ?;");
-			preparedStatement.setString(1, user);
-			ResultSet e = preparedStatement.executeQuery();
+			PreparedStatement e = conn.prepareStatement("SELECT * FROM users WHERE user = ?;");
+			e.setString(1, user);
+			ResultSet r = e.executeQuery();
 			ArrayList<String> item = new ArrayList<String>();
-			if (e.next()) {
-				item.add(e.getString("user"));
-				item.add(e.getString("time"));
+			if (r.next()) {
+				item.add(r.getString("user"));
+				item.add(r.getString("time"));
 				e.close();
 				return item;
 			}
--- src/Example/db/MySQL.java
+++ src/Example/db/MySQL.java
@@ -4,13 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 public class MySQL implements Database {
 	private Connection conn;
-	private Statement statmt;
-	private PreparedStatement preparedStatement = null;
 
 	public MySQL(String url, String dbName, String user, String pass) {
 		try {
@@ -18,9 +15,8 @@ public class MySQL implements Database {
 			conn = DriverManager.getConnection(
 					"jdbc:mysql://" + url + "/" + dbName + "?useUnicode=true&characterEncoding=utf8&autoReconnect=true",
 					user, pass);
-			statmt = conn.createStatement();
-			statmt.execute(
-					"CREATE TABLE IF NOT EXISTS `users` (`user` varchar(255) PRIMARY KEY,`time` varchar(255) NOT NULL)");
+			conn.createStatement().execute(
+					"CREATE TABLE IF NOT EXISTS `users` (`user` varchar(16) PRIMARY KEY,`time` varchar(255) NOT NULL)");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -43,13 +39,13 @@ public class MySQL implements Database {
 	@Override
 	public ArrayList<String> select(String user) {
 		try {
-			preparedStatement = conn.prepareStatement("SELECT * FROM users WHERE user = ?;");
-			preparedStatement.setString(1, user);
-			ResultSet e = preparedStatement.executeQuery();
+			PreparedStatement e = conn.prepareStatement("SELECT * FROM users WHERE user = ?;");
+			e.setString(1, user);
+			ResultSet r = e.executeQuery();
 			ArrayList<String> item = new ArrayList<String>();
-			if (e.next()) {
-				item.add(e.getString("user"));
-				item.add(e.getString("time"));
+			if (r.next()) {
+				item.add(r.getString("user"));
+				item.add(r.getString("time"));
 				e.close();
 				return item;
 			}
