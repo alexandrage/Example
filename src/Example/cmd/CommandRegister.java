ce1f6c3673ed37d519ddcaf143de1679a2f27338
alexandrage <alexandrage@mail.ru>
2020-08-15 18:57:25 +0300

    up

--- src/Example/cmd/CommandRegister.java
+++ src/Example/cmd/CommandRegister.java
@@ -1,9 +1,9 @@
 package Example.cmd;
 
+import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Map;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -12,6 +12,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginIdentifiableCommand;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
 
 public class CommandRegister extends Command implements PluginIdentifiableCommand {
 	protected Plugin plugin;
@@ -44,32 +45,27 @@ public class CommandRegister extends Command implements PluginIdentifiableComman
 			e.printStackTrace();
 		}
 	}
-	
-	public static void clearCommand() {
+
+	public static void unregister(String name) {
 		try {
 			Field field = Bukkit.getServer().getClass().getDeclaredField("commandMap");
 			field.setAccessible(true);
 			CommandMap map = (CommandMap) field.get(Bukkit.getServer());
-            Field field2 = SimpleCommandMap.class.getDeclaredField("knownCommands");
-            field2.setAccessible(true);
-            Map<String, Command> command = (Map<String, Command>) field2.get(map);
-            command.clear();
+			Field field2 = SimpleCommandMap.class.getDeclaredField("knownCommands");
+			field2.setAccessible(true);
+			@SuppressWarnings("unchecked")
+			Map<String, Command> command = (Map<String, Command>) field2.get(map);
+			Command cmd = command.get(name);
+			InputStream stream = cmd.getClass().getResourceAsStream("/plugin.yml");
+			PluginDescriptionFile desc = new PluginDescriptionFile(stream);
+			command.remove(cmd.getName());
+			command.remove(desc.getName().toLowerCase() + ":" + cmd.getName());
+			for (String aliases : cmd.getAliases()) {
+				command.remove(aliases);
+				command.remove(desc.getName().toLowerCase() + ":" + aliases);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static Map<String, Command> getCommand() {
-		try {
-			Field field = Bukkit.getServer().getClass().getDeclaredField("commandMap");
-			field.setAccessible(true);
-			CommandMap map = (CommandMap) field.get(Bukkit.getServer());
-            Field field2 = SimpleCommandMap.class.getDeclaredField("knownCommands");
-            field2.setAccessible(true);
-            return (Map<String, Command>) field2.get(map);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 }
